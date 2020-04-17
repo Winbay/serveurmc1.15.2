fc4f2b352e0d10fb8a861a412c60e6bfcecc44df
Antoine Tran <antoine2001.tran@gmail.com>
2020-04-17 19:42:32 +0200

    Initial commit

---
+++ src/main/java/net/akabay/mcserver/core/MainPlugin.java
@@ -0,0 +1,11 @@
+package net.akabay.mcserver.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Plugin successfully enabled");
+    }
+}
