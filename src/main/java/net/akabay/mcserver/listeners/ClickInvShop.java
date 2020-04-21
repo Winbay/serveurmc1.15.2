37f2cc7c478f59e80feb3329cc40b30f0d6c1b62
Pc fixe Vincent Barbier <winbaytb@gmail.com>
2020-04-21 23:27:07 +0200

    add PassNight

---
+++ src/main/java/net/akabay/mcserver/listeners/ClickInvShop.java
@@ -0,0 +1,14 @@
+package net.akabay.mcserver.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class ClickInvShop implements Listener {
+    @EventHandler
+    public void onClickInventoryEvent(InventoryClickEvent event){
+        if(event.getView().getTitle().equals("Shop")){
+            event.setCancelled(true);
+        }
+    }
+}
--- src/main/java/net/akabay/mcserver/listeners/JoinLeaveListener.java
+++ src/main/java/net/akabay/mcserver/listeners/JoinLeaveListener.java
@@ -1,6 +1,7 @@
 package net.akabay.mcserver.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,11 +11,11 @@ public class JoinLeaveListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        event.setJoinMessage("§6" + event.getPlayer().getName() + "§a vient de se connecter.");
+        event.setJoinMessage(ChatColor.YELLOW + event.getPlayer().getDisplayName() + ChatColor.GREEN + " vient de se connecter.");
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event){
-        event.setQuitMessage("§6"+ event.getPlayer().getName() +"§c vient de se deconnecter.");
+        event.setQuitMessage(ChatColor.YELLOW + event.getPlayer().getDisplayName() + ChatColor.RED +" vient de se deconnecter.");
     }
 }
